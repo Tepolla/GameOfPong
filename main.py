847d5ef9f39bf232c9678509acfd14ba0cec7cce
Arden <123322851+Tepolla@users.noreply.github.com>
2024-02-28 15:45:21 -0500

    P1 Paddle added for user control

--- main.py
+++ main.py
@@ -1,135 +1,91 @@
 import pygame
 import sys
-import random
-import math
 
 ################################ Global variables ################################
-#region
 
 # Border items
 borderTop = 0
 borderLeft = 0
-borderBottom = 700  # Screen height
-borderRight = 1000  # Screen width
+borderBottom = 700 # Height of the screen
+borderRight = 1000 # Width of the screen
 
 # Paddle items
 PADDLE_WIDTH, PADDLE_HEIGHT = 15, 90
-PADDLE_MOVEMENT_DISTANCE = 10
+PADDLE_MOVEMENT_DISTANCE = 20
 
-# Player items
+# Player1 items
 P1Score = 0
+P1PaddleX = borderLeft + 10 # borderLeft isn't needed, it just helps make the code look neater
+P1PaddleY = borderBottom / 2
+
+# Player2 items
 P2Score = 0
-P1PaddleY = borderBottom / 2 - PADDLE_HEIGHT / 2
-P2PaddleY = borderBottom / 2 - PADDLE_HEIGHT / 2
+P2PaddleX = borderRight - 10
+P2PaddleY = borderBottom / 2
 
 # Ball items
-ballX = borderRight // 2
-ballY = borderBottom // 2
-ballSpeed = 5
-ballRadius = 10
-ballSpeedX = 0  # Initialize with a default value
-ballSpeedY = 0  # Initialize with a default value
+ballX = 0
+ballY = 0
+ballDir = 0
+ballSpeed = 0
 
-# Frame rate control
-FPS = 60
 
-#endregion
 ################################ Global variables ################################
 
+
+
 ################################ Paddle mechanics ################################
-#region
 
 def Paddle(state, y):
-    if y + PADDLE_HEIGHT >= borderBottom and state == 1:
-        return 0
-    elif y <= borderTop and state == -1:
-        return 0
-    else:
-        return PADDLE_MOVEMENT_DISTANCE * state
+    # REMEMBER, (0, 0) IS LOCATED AT THE TOP LEFT OF THE CREATED WINDOW
+    # THEREFORE INCREASING Y POSITION MOVES ENTITY DOWN AND DECREASING, UP
 
-#endregion
+    # Prevents paddle from going below the screen
+    if y >= borderBottom and state == -1:
+        return 0
+    # Prevents paddle from going above the screen
+    elif y <= borderTop and state == 1:
+        return 0
+    #
+    else:
+        # Moves paddle up
+        if state == 1:
+            return -PADDLE_MOVEMENT_DISTANCE
+        # Moves paddle down
+        elif state == -1:
+            return PADDLE_MOVEMENT_DISTANCE
+        # Doesn't move paddle if state is 0 (no directional input)
+        else:
+            return 0
+
+
 ################################ Paddle mechanics ################################
 
-################################ P1 & P2 paddle mechanics ################################
-#region
 
-def P1PaddleMove(state):
+
+################################ P1 paddle mechanics ################################
+
+def P1PaddleMove():
     global P1PaddleY
-    P1PaddleY += Paddle(state, P1PaddleY)
 
-def P2PaddleMove(state):
-    global P2PaddleY
-    P2PaddleY += Paddle(state, P2PaddleY)
-
-#endregion
-################################ P1 & P2 paddle mechanics ################################
-
-################################ Ball mechanics ################################
-#region
-
-def reset_ball():
-    global ballX, ballY, ballSpeedX, ballSpeedY
-    ballX = borderRight // 2
-    ballY = borderBottom // 2
-    angle = random.choice([random.randint(-45, 45), random.randint(135, 225)])
-    angle_radians = math.radians(angle)
-    ballSpeedX = ballSpeed * math.cos(angle_radians)
-    ballSpeedY = ballSpeed * math.sin(angle_radians)
-
-def move_ball():
-    global ballX, ballY, P1Score, P2Score, ballSpeedX, ballSpeedY
-
-    ballX += ballSpeedX
-    ballY += ballSpeedY
-
-    # Collision with top and bottom borders
-    if ballY - ballRadius <= borderTop or ballY + ballRadius >= borderBottom:
-        ballY -= ballSpeedY
-        ballSpeedY *= -1
-
-    # Collision with paddles
-    if ballX - ballRadius <= PADDLE_WIDTH and P1PaddleY <= ballY <= P1PaddleY + PADDLE_HEIGHT:
-        ballX += ballSpeedX
-        ballSpeedX *= -1
-    elif ballX + ballRadius >= borderRight - PADDLE_WIDTH and P2PaddleY <= ballY <= P2PaddleY + PADDLE_HEIGHT:
-        ballX -= ballSpeedX
-        ballSpeedX *= -1
-
-    # Scoring
-    if ballX <= borderLeft:
-        P2Score += 1
-        reset_ball()
-    elif ballX >= borderRight:
-        P1Score += 1
-        reset_ball()
-
-
-
-#endregion
-################################ Ball mechanics ################################
-
-################################ User controlled mechanics ################################
-#region
-
-def getUserInput():
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        P1PaddleMove(-1)
-    if keys[pygame.K_s]:
-        P1PaddleMove(1)
-    if keys[pygame.K_UP]:
-        P2PaddleMove(-1)
-    if keys[pygame.K_DOWN]:
-        P2PaddleMove(1)
 
-#endregion
-################################ User controlled mechanics ################################
+    if keys[pygame.K_w]:
+        P1PaddleY += Paddle(1, P1PaddleY)
+    if keys[pygame.K_s]:
+        P1PaddleY += Paddle(-1, P1PaddleY)
+
+
+################################ P1 paddle mechanics ################################
+
+
 
 # Initialize Pygame
 pygame.init()
+
+# Setting the width and height of the screen
 screen = pygame.display.set_mode((borderRight, borderBottom))
 pygame.display.set_caption("Pong Game")
-clock = pygame.time.Clock()
 
 # Main game loop
 while True:
@@ -138,20 +94,5 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Get input for user 1 and 2
-    getUserInput()
-
-    # Move the ball
-    move_ball()
-
-    # Draw everything
-    screen.fill((0, 0, 0))  # Clear screen with black color
-    pygame.draw.rect(screen, (255, 255, 255), (0, P1PaddleY, PADDLE_WIDTH, PADDLE_HEIGHT))  # Player 1 paddle
-    pygame.draw.rect(screen, (255, 255, 255), (borderRight - PADDLE_WIDTH, P2PaddleY, PADDLE_WIDTH, PADDLE_HEIGHT))  # Player 2 paddle
-    pygame.draw.circle(screen, (255, 255, 255), (int(ballX), int(ballY)), ballRadius)  # Ball
-
     # Update screen
     pygame.display.flip()
-
-    # FPS control
-    clock.tick(FPS)
