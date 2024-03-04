968c981d2efbb6c45de35a9dfb6d3e03e1d87009
Arden <123322851+Tepolla@users.noreply.github.com>
2024-03-03 19:42:48 -0500

    Draw & Game method added to reduce main functionality

--- main.py
+++ main.py
@@ -7,34 +7,32 @@ import sys
 # Border items
 borderTop = 0
 borderLeft = 0
-borderBottom = 700  # Height of the screen
-borderRight = 1000  # Width of the screen
+borderBottom = 700  # Screen height
+borderRight = 1000  # Screen width
 
 # Paddle items
 PADDLE_WIDTH, PADDLE_HEIGHT = 15, 90
 PADDLE_MOVEMENT_DISTANCE = 10
 
-# Player1 items
+# Player items
 P1Score = 0
-P1PaddleX = borderLeft + 10  # borderLeft isn't needed, it just helps make the code look neater
-P1PaddleY = borderBottom / 2
-
-# Player2 items
 P2Score = 0
-P2PaddleX = borderRight - PADDLE_WIDTH - 10
-P2PaddleY = borderBottom / 2
+P1PaddleY = borderBottom / 2 - PADDLE_HEIGHT / 2
+P2PaddleY = borderBottom / 2 - PADDLE_HEIGHT / 2
 
 # Ball items
-ballX = 0
-ballY = 0
-ballDir = 0
-ballSpeed = 0
+ballX = borderRight // 2
+ballY = borderBottom // 2
+ballSpeed = 5
+ballRadius = 10
+ballSpeedX = 0  # Initialize with a default value
+ballSpeedY = 0  # Initialize with a default value
 
 # Frame rate control
 FPS = 60
 
-#endregion
 ################################ Global variables ################################
+#endregion
 
 
 
@@ -42,119 +40,92 @@ FPS = 60
 #region
 
 def Paddle(state, y):
-    # REMEMBER, (0, 0) IS LOCATED AT THE TOP LEFT OF THE CREATED WINDOW
-    # THEREFORE INCREASING Y POSITION MOVES ENTITY DOWN AND DECREASING, UP
-
-    # Prevents paddle from going below the screen
-    if (y + PADDLE_HEIGHT) >= borderBottom and state == -1:
+    if y + PADDLE_HEIGHT >= borderBottom and state == 1:
         return 0
-    # Prevents paddle from going above the screen
-    elif y <= borderTop and state == 1:
+    elif y <= borderTop and state == -1:
         return 0
-    #
     else:
-        # Moves paddle up
-        if state == 1:
-            return -PADDLE_MOVEMENT_DISTANCE
-        # Moves paddle down
-        elif state == -1:
-            return PADDLE_MOVEMENT_DISTANCE
-        # Doesn't move paddle if state is 0 (no directional input)
-        else:
-            return 0
+        return PADDLE_MOVEMENT_DISTANCE * state
 
-#endregion
 ################################ Paddle mechanics ################################
+#endregion
 
 
 
 ################################ P1 & P2 paddle mechanics ################################
 #region
-
-#------------------ P1 paddle ------------------#
 
 def P1PaddleMove(state):
     global P1PaddleY
     P1PaddleY += Paddle(state, P1PaddleY)
 
-#------------------ P2 paddle ------------------#
-
 def P2PaddleMove(state):
     global P2PaddleY
     P2PaddleY += Paddle(state, P2PaddleY)
 
-#endregion
 ################################ P1 & P2 paddle mechanics ################################
+#endregion
 
 
 
 ################################ User controlled mechanics ################################
 #region
 
-#------------------ P1 controls ------------------#
-def getUser1Input():
+def getUserInput():
     keys = pygame.key.get_pressed()
-
-    # Player 1 controls
     if keys[pygame.K_w]:
-        P1PaddleMove(1)  # Move up
+        P1PaddleMove(-1)
     if keys[pygame.K_s]:
-        P1PaddleMove(-1)  # Move down
-
-#------------------ P2 controls ------------------#
-def getUser2Input():
-    keys = pygame.key.get_pressed()
-
-    # Player 2 controls
+        P1PaddleMove(1)
     if keys[pygame.K_UP]:
-        P2PaddleMove(1)  # Move up
+        P2PaddleMove(-1)
     if keys[pygame.K_DOWN]:
-        P2PaddleMove(-1)  # Move down
+        P2PaddleMove(1)
 
-#endregion
 ################################ User controlled mechanics ################################
-
-
-
-# Initialize Pygame
-#region
-
-pygame.init()
-
-# Setting the width and height of the screen
-screen = pygame.display.set_mode((borderRight, borderBottom))
-pygame.display.set_caption("Pong Game")
-
-# Clock object to control the frame rate
-clock = pygame.time.Clock()
-
 #endregion
 
-# Main game loop
+
+
+################################ Draw Method ################################
 #region
 
-while True:
+def draw():
+    screen.fill((0, 0, 0))  # Clear screen with black color
+    pygame.draw.rect(screen, (255, 255, 255), (0, P1PaddleY, PADDLE_WIDTH, PADDLE_HEIGHT))  # Player 1 paddle
+    pygame.draw.rect(screen, (255, 255, 255),
+                     (borderRight - PADDLE_WIDTH, P2PaddleY, PADDLE_WIDTH, PADDLE_HEIGHT))  # Player 2 paddle
+    pygame.display.flip()
+
+################################ Draw Method ################################
+#endregion
+
+
+
+################################ Game Method ################################
+#region
+
+def game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # Get user input
-    getUser1Input()
+    getUserInput()
+    draw()
 
-    # Draw everything here
-    screen.fill((0, 0, 0))  # Clear screen with black color
-
-    # Draw Player 1 paddle
-    pygame.draw.rect(screen, (255, 255, 255), (P1PaddleX, P1PaddleY, PADDLE_WIDTH, PADDLE_HEIGHT))
-
-    # Draw Player 2 paddle
-    pygame.draw.rect(screen, (255, 255, 255), (P2PaddleX, P2PaddleY, PADDLE_WIDTH, PADDLE_HEIGHT))
-
-    # Update screen
-    pygame.display.flip()
-
-    # FPS control to make movements smoother
-    clock.tick(FPS)
-
+################################ Game Method ################################
 #endregion
+
+
+
+# Initialize Pygame
+pygame.init()
+screen = pygame.display.set_mode((borderRight, borderBottom))
+pygame.display.set_caption("Pong Game")
+clock = pygame.time.Clock()
+
+# Main game loop
+while True:
+    game()
+    clock.tick(FPS)
